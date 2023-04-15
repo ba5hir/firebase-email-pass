10b2522a1187b0cbe0d6421ea0c9dae3f6d7794b
ba5hir <bashir15-12342@diu.edu.bd>
2023-04-15 12:30:06 +0600

    form onsubmit

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,27 @@
+import { config } from "localforage";
 import React from "react";
+
+const handleEmailChange = (event) => {
+  // console.log(event.target.value);
+};
+const handlePassBlur = (event) => {
+  // console.log(event.target.value);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const pass = event.target.password.value;
+  console.log(email, pass);
+};
 
 const Register = () => {
   return (
     <div>
       <h2>Please Register</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
+          onChange={handleEmailChange}
           type="email"
           name="email"
           id="email"
@@ -13,6 +29,7 @@ const Register = () => {
         />
         <br />
         <input
+          onBlur={handlePassBlur}
           type="password"
           name="Password"
           id="password"
